ce9485e8efbe6debbb70174412cd2237a15b6023
kyo-qin <kyo-qin@163.com>
2018-04-09 11:36:29 +0800

    thymeleaf

---
+++ panda-customer-thymeleaf/src/main/java/org/panda/customer/thymeleaf/App.java
@@ -0,0 +1,18 @@
+package org.panda.customer.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+// @EnableEurekaClient
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
