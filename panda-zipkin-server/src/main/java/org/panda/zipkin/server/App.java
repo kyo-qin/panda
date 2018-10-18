72084bd44feab0f57187d8a6a9ad836ee096780c
kyo-qin <kyo-qin@163.com>
2018-10-18 09:03:14 +0800

    add new

--- panda-zipkin-server/src/main/java/org/panda/zipkin/server/App.java
+++ panda-zipkin-server/src/main/java/org/panda/zipkin/server/App.java
@@ -2,6 +2,7 @@ package org.panda.zipkin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import zipkin.server.EnableZipkinServer;
 
@@ -11,6 +12,7 @@ import zipkin.server.EnableZipkinServer;
  */
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
